d2c40490c8857112ef8b81e363928bac3ddde7da
brojor <bronislav.jordan@gmail.com>
2021-07-02 18:26:38 +0200

    header přizpůsoben novému designu

--- src/components/AuditStoreHeader.vue
+++ src/components/AuditStoreHeader.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="header">
-    <img class="logo" src="@/assets/Rock_Point_logo.jpg" alt="" />
-    <h1>
-      <span>R</span>OCK <span>P</span>OINT- <span>S</span>TORE
-      <span>A</span>UDIT
-    </h1>
+    <img class="logo" src="@/assets/logo-small.png" alt="logo" />
+    <h1>STORE AUDIT</h1>
   </div>
 </template>
 
@@ -19,24 +16,31 @@ export default {
 </script>
 
 <style lang="css" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarpanch:wght@700&display=swap');
+
 img {
-  width: 4.5rem;
-  height: 4.5rem;
+  height: 3rem;
+  margin-top: 4px;
 }
 div.header {
   display: flex;
   justify-content: center;
+  align-items: center;
   padding: 0.5rem;
   width: 100vw;
-  background: white;
+  background: #292929;
+  /* background: #002927; */
   margin-bottom: 2rem;
   box-shadow: 0px 0px 20px 3px rgba(0, 0, 0, 0.64);
   border-bottom: 1px solid black;
+  color: #fff;
 }
 .header h1 {
-  margin-left: 2rem;
+  margin-left: 0.75rem;
   margin-top: 2px;
   font-family: 'Orbitron', sans-serif;
+  font-family: 'Sarpanch', sans-serif;
+  font-style: italic;
   font-size: 2.9rem;
   letter-spacing: 2px;
 }
